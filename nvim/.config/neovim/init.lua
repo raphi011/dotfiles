ff6e21fe5eb9d81b0fafae7f02693bedff2bc7c3
Raphael Gruber <raphael.gruber@form3.tech>
2023-07-06 10:03:57 +0200

    add more nvim plugins

--- nvim/.config/neovim/init.lua
+++
@@ -1,6 +0,0 @@
-vim.g.mapleader = " "
-vim.opt.tabstop = 3
-vim.opt.shiftwidth = 3
-vim.opt.expandtab = true
-vim.opt.number = true
-vim.opt.smartcase = true
--- nvim/.config/nvim/lua/conf/packer.lua
+++ nvim/.config/nvim/lua/conf/packer.lua
@@ -2,7 +2,6 @@
 
 -- Only required if you have packer configured as `opt`
 vim.cmd [[packadd packer.nvim]]
-
 return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
@@ -12,5 +11,18 @@ return require('packer').startup(function(use)
      -- or                            , branch = '0.1.x',
      requires = { {'nvim-lua/plenary.nvim'} }
   }
+
+  use({
+     'rose-pine/neovim',
+     as = 'rose-pine',
+     config = function()
+        vim.cmd('colorscheme rose-pine')
+     end
+  })
+
+
+  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+  use('mbbill/undotree')
+  use('tpope/vim-fugitive')
 
 end)
