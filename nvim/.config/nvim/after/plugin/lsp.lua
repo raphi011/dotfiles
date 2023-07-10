b76f84599e28c7bb4efff19220281c0eb7a06c8c
Raphael Gruber <raphael.gruber@form3.tech>
2023-07-10 15:49:29 +0200

    make sure gopls language server is installed

--- nvim/.config/nvim/after/plugin/lsp.lua
+++ nvim/.config/nvim/after/plugin/lsp.lua
@@ -4,6 +4,10 @@ lsp.on_attach(function(client, bufnr)
    lsp.default_keymaps({buffer = bufnr})
 end)
 
+lsp.ensure_installed({
+   'gopls'
+})
+
 --T (Optional) Configure lua language server for neovim
 require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
 
