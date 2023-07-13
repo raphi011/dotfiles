269a6a64d0f0f9647adf59ff9c041cd50b553acd
Raphael Gruber <raphi011@gmail.com>
2023-07-13 21:50:25 +0200

    add bufferline nvim plugin

--- nvim/.config/nvim/lua/conf/packer.lua
+++ nvim/.config/nvim/lua/conf/packer.lua
@@ -49,4 +49,6 @@ return require('packer').startup(function(use)
 
    use {'nvim-lualine/lualine.nvim', requires = { 'nvim-tree/nvim-web-devicons', opt = true }}
 
+   use {'akinsho/bufferline.nvim', tag = "*", requires = 'nvim-tree/nvim-web-devicons'}
+
 end)
--- nvim/.config/nvim/lua/conf/remap.lua
+++ nvim/.config/nvim/lua/conf/remap.lua
@@ -22,3 +22,5 @@ vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+
+vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
