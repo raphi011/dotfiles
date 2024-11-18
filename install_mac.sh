b1b990a8422b88d63dfa94265a315e8cd561cace
Raphael Gruber <raphi011@gmail.com>
2024-11-18 22:17:51 +0100

    update install.sh

--- install_mac.sh
+++ install_mac.sh
@@ -2,4 +2,6 @@
 
 $(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)
 
-brew install go k9s htop nvim fzf starship rg exa gh fish yq
+brew install go k9s htop nvim fzf rg exa gh fish fisher yq diff-so-fancy kitty glow bat dlv ranger hexyl
+
+fisher install IlanCosman/tide@v6
